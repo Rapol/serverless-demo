4fb94f9ce589425def3d90a0759e3b403060680d
Rafael Pol <rapol32@gmail.com>
2018-07-23 21:28:40 -0400

    adding function definition

--- src/palindrome/index.js
+++ src/palindrome/index.js
@@ -2,5 +2,10 @@ module.exports.handler = async (event, context) => {
     console.log(`EVENT = ${JSON.stringify(event, null, 4)}`);
     const message = 'Hello Serverless World!';
     console.log(message);
-    return message;
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message,
+        }),
+    };
 };
